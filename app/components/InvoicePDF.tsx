64e205371e459e4075707637410c1bb5b81ebdaa
Ibrahima SARR <91768858+kisweb@users.noreply.github.com>
2025-10-27 10:09:21 +0000

    Update InvoicePDF format

--- app/components/InvoicePDF.tsx
+++ app/components/InvoicePDF.tsx
@@ -40,7 +40,7 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
         const pdfWidth = pdf.internal.pageSize.getWidth();
         const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
 
-        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+        pdf.addImage(imgData, "PNG", 0, 0, 210, 297) //pdfWidth, pdfHeight);
         pdf.save(`facture-${invoice.id}-${invoice.name}.pdf`);
 
         confetti({
@@ -56,8 +56,8 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
   };
 
   return (
-    <div className="mt-4 hidden lg:block">
-      <div className="border-base-300 border-2 border-dashed rounded-xl p-5">
+    <div className="mt-4 block md:block">
+      <div className="border-base-300 border-2 border-dashed rounded-xl p-2">
         <button
           onClick={handleDownloadPdf}
           className="btn btn-sm btn-accent mb4"
@@ -65,10 +65,9 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
           Facture PDF
           <ArrowDownFromLine className="w-4" />
         </button>
-
         <div className="p-8" ref={factureRef}>
           <div className="flex justify-between items-center text-sm">
-            <div className="flex flex-col border-1 rounded-xl p-2">
+            <div className="flex flex-col border-1 rounded-xl p-1">
               <div className="flex items-center rounded-xl">
                 <Image
                   src="/taureau1.png"
@@ -78,24 +77,26 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
                   className="ml-2 z-0 rounded-full bottom-2 border-spacing-2 shadow-2xl"
                 />
                 <div className="relative ml-3 font-bold text-xl border-3 border-lime-500 italic z-50">
-                  <span className="text-2xl font-bold italic">
+                  <span className="text-xl font-bold italic">
                     Kis@rr<span className="text-orange-500">-WEB</span>
                   </span>
                   <div className="absolute top-[0] left-[70%] bg-transparent text-orange-500  rounded-3xl p-1">
                     <SatelliteDish className="h-8 w-8" />
                   </div>
-                  <hr className="border-b-2 border-orange-500 w-[160px]"/>
+                  <hr className="border-b-2 border-orange-500 w-[160px]" />
                   <span className="text-md font-light">
-                    <p className="mt-1">sarrsindian@gmail.com</p>
-                    <span className="mt-0">77 554 41 91</span>
+                    <p className="mt-1 text-sm">sarrsindian@gmail.com</p>
                   </span>
+                  <p className="mt-2">Woury Shop - Bignona</p>
                 </div>
               </div>
-              <h1 className="text-4xl mt-4 font-bold">Facture</h1>
+              {/* <h1 className="text-4xl mt-4 font-bold">Facture</h1> */}
             </div>
             <div className="text-right">
-              <p className="badge badge-info mb-2 px-2 py-4 text-pretty font-bold ">Facture n° : {invoice.id}</p>
-              <p className="my-2">
+              <p className="badge badge-info mb-2 px-2 py-3 text-pretty font-bold ">
+                Facture n° : {invoice.id}
+              </p>
+              <p className="my-1">
                 <strong>Date </strong>
                 {formatDate(invoice.invoiceDate)}
               </p>
@@ -103,28 +104,15 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
                 <strong>Date d&apos;échéance </strong>
                 {formatDate(invoice.dueDate)}
               </p>
-            </div>
-          </div>
-
-          <div className="my-6 flex justify-between">
-            <div>
-              <p className="badge badge-ghost mb-2">Émetteur</p>
-              <p className="text-sm font-bold italic">{invoice.issuerName}</p>
-              <p className="text-sm text-gray-500 w-52 break-words">
-                {invoice.issuerAddress}
-              </p>
-            </div>
-            <div className="text-right">
-              <p className="badge badge-ghost mb-2">Client</p>
-              <p className="text-sm font-bold italic">{invoice.clientName}</p>
-              <p className="text-sm text-gray-500 w-52 break-words">
-                {invoice.clientAddress}
+              <p className="my-4">
+                <strong>Client  </strong> {invoice.clientName} - {invoice.clientAddress}
               </p>
             </div>
           </div>
-
+          <hr />
+          <hr className="border border-2 border-gray-900" />
           <div className="overflow-x-auto">
-            <table className="table table-zebra">
+            <table className="table table-zebra text-sm">
               <thead>
                 <tr>
                   <th></th>
@@ -134,14 +122,18 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
                   <th>Total</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="text-sm">
                 {invoice.lines.map((ligne, index) => (
                   <tr key={index + 1}>
-                    <td>{index + 1}</td>
-                    <td>{ligne.description}</td>
-                    <td>{ligne.quantity}</td>
-                    <td>{ligne.unitPrice.toFixed(2)} xof</td>
-                    <td>{(ligne.quantity * ligne.unitPrice).toFixed(2)} xof</td>
+                    <td className="mb-1 py-0">{index + 1}</td>
+                    <td className="mb-1 py-0">{ligne.description}</td>
+                    <td className="mb-1 py-0">{ligne.quantity}</td>
+                    <td className="mb-1 py-0">
+                      {ligne.unitPrice }
+                    </td>
+                    <td className="mb-1 py-1">
+                      {(ligne.quantity > 0) ? (ligne.quantity * ligne.unitPrice) : ''}
+                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -149,22 +141,24 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
           </div>
 
           <div className="mt-6 space-y-2 text-md">
-            <div className="flex justify-between">
+            {/* <div className="flex justify-between">
               <div className="font-bold">Total Hors Taxes</div>
-              <div>{totals.totalHT.toFixed(2)} xof</div>
-            </div>
+              <div>{totals.totalHT.toFixed(0)} xof</div>
+            </div> */}
 
-            {invoice.vatActive && (
+            {/* {invoice.vatActive && (
               <div className="flex justify-between">
                 <div className="font-bold">TVA {invoice.vatRate} %</div>
-                <div>{totals.totalVAT.toFixed(2)} xof</div>
+                <div>{totals.totalVAT.toFixed(0)} xof</div>
               </div>
-            )}
+            )} */}
 
             <div className="flex justify-between">
-              <div className="font-bold">Total Toutes Taxes Comprises</div>
-              <div className="badge badge-accent">
-                {totals.totalTTC.toFixed(2)} xof
+              <div className="font-bold">Total à payer</div>
+              <div className="badge bg-black px-4 py-1">
+                <p className="badge badge-white mb-1 p-2 text-pretty font-bold">
+                  {totals.totalTTC.toFixed(0)} xof
+                </p>
               </div>
             </div>
           </div>
