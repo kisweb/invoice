e1851f3e88e1e09d84017a7d763efbf38a9fb16f
Ibrahima SARR <91768858+kisweb@users.noreply.github.com>
2025-10-27 21:24:09 +0000

    pdf modified

--- app/components/InvoicePDF.tsx
+++ app/components/InvoicePDF.tsx
@@ -37,10 +37,8 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
           format: "A4",
         });
 
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
 
-        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+        pdf.addImage(imgData, "PNG", 0, 0, 210, 297);
         pdf.save(`facture-${invoice.id}-${invoice.name}.pdf`);
 
         confetti({
